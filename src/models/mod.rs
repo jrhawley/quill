6946663856db687bee7ed4b8e9bb976160f37e3d
James Hawley <jamesrichardhawley@gmail.com>
2020-12-19 23:50:47 -0500

    Adding `statement` submodule and `Statement` structu

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,3 +1,4 @@
 pub mod account;
-pub mod institution;
 pub mod date;
+pub mod institution;
+pub mod statement;
---
+++ src/models/statement.rs
@@ -0,0 +1,37 @@
+use crate::models::date::Date;
+use std::path::{Path, PathBuf};
+
+pub struct Statement {
+    path: PathBuf,
+    date: Date,
+}
+
+impl Statement {
+    /// Construct a new Statement
+    pub fn new(path: PathBuf, date: Date) -> Statement {
+        Statement { path, date }
+    }
+
+    /// Access the date
+    pub fn date(&self) -> Date {
+        self.date
+    }
+
+    /// Access the statement path
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+
+    /// Construct Statement from a file
+    pub fn from_path(path: &Path, fmt: &str) -> Statement {
+        // default to be used with parsing errors
+        let false_date = Date::from_ymd(1900, 01, 01);
+        let date = Date::parse_from_str(path.file_stem().unwrap().to_str().unwrap(), fmt)
+            .unwrap_or(false_date);
+
+        Statement {
+            path: PathBuf::from(path),
+            date,
+        }
+    }
+}
