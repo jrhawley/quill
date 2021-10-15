57072a8a380d046b03122676854b9b4c19968f04
James Hawley <james@jrhawley.ca>
2021-10-15 01:21:47 -0400

    Adding function to expand tildes
    The error in parsing the directory from a config file with `~`
    characters was causing unknown problems.
    But using the `dirs` crate and canonicalizing paths in a safer way has
    fixed these problems.

--- src/config/utils.rs
+++ src/config/utils.rs
@@ -1,7 +1,9 @@
 //! Utilities to load, parse, and manage the configuration.
 
 use clap::crate_name;
+use dirs;
 use home::home_dir;
+use std::path::Path;
 use std::{env, io::Result, path::PathBuf};
 use toml::map::Map;
 use toml::Value;
@@ -51,4 +53,26 @@ pub fn get_config_path() -> PathBuf {
             }
         }
     }
+}
+
+/// Replace the `~` character in any path with the home directory
+/// See https://stackoverflow.com/a/54306906/7416009
+pub fn expand_tilde<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    let p = path.as_ref();
+    if !p.starts_with("~") {
+        return Some(p.to_path_buf());
+    }
+    if p == Path::new("~") {
+        return home_dir();
+    }
+    dirs::home_dir().map(|mut h| {
+        if h == Path::new("/") {
+            // base case: `h` root directory;
+            // don't prepend extra `/`, just drop the tilde.
+            p.strip_prefix("~").unwrap().to_path_buf()
+        } else {
+            h.push(p.strip_prefix("~/").unwrap());
+            h
+        }
+    })
 }
