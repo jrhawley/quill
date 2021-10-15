e88b3cd5b48fc31872930042e586a8b98d145156
James Hawley <james@jrhawley.ca>
2021-10-15 00:26:46 -0400

    Refactoring config module

---
+++ src/config/config.rs
@@ -0,0 +1,130 @@
+//! Global account configuration details.
+
+use kronos::{step_by, Grain, Grains, LastOf, NthOf, Shim};
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{Error, ErrorKind};
+use std::path::{Path, PathBuf};
+use toml::Value;
+
+use crate::{config::utils::parse_accounts, models::account::Account};
+
+/// Account and program configuration
+#[derive(Debug)]
+pub struct Config<'a> {
+    // absolute path of the config file
+    path: PathBuf,
+    // account information
+    accounts: HashMap<String, Account<'a>>,
+}
+
+impl<'a> Config<'a> {
+    /// Attempt to load and parse the config file into our Config struct.
+    /// If a file cannot be found, return a default Config.
+    /// If we find a file but cannot parse it, panic
+    pub fn new_from_path(path: &Path) -> Result<Config<'a>, Error> {
+        // placeholder for config string contents
+        let mut config_str = String::new();
+        // config to be returned, if parsed properly
+        let mut conf = Config {
+            path: PathBuf::from(path),
+            accounts: HashMap::new(),
+        };
+
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(e) => {
+                return Err(Error::new(ErrorKind::NotFound, e));
+            }
+        };
+
+        // read file contents and assign to config_toml
+        match file.read_to_string(&mut config_str) {
+            Ok(_) => {}
+            Err(e) => return Err(Error::new(ErrorKind::InvalidData, e)),
+        }
+
+        let config_toml = match config_str.parse() {
+            Ok(Value::Table(s)) => s,
+            Ok(_) => {
+                return Err(Error::new(
+                    ErrorKind::InvalidInput,
+                    "Error parsing config TOML table.",
+                ))
+            }
+            Err(e) => return Err(Error::new(ErrorKind::InvalidInput, e)),
+        };
+        // parse accounts
+        if let Some(Value::Table(table)) = config_toml.get("Accounts") {
+            parse_accounts(table, &mut conf);
+        }
+        Ok(conf)
+    }
+
+    /// Get the path of the config file
+    /// By `new` implementation, it is assured that this is an absolute path
+    pub fn path(&self) -> &Path {
+        &self.path.as_path()
+    }
+
+    /// Get the list of accounts in the configuration
+    pub fn accounts(&self) -> &HashMap<String, Account<'a>> {
+        // return required here becuase of the pointer
+        return &self.accounts;
+    }
+
+    /// Get the list of account names in the configuration, sorted by name
+    pub fn accounts_sorted(&self) -> (Vec<&str>, Vec<&str>) {
+        // collect account keys
+        let mut v = self
+            .accounts()
+            .iter()
+            .map(|(k, _)| k.as_str())
+            .collect::<Vec<&str>>();
+        // sort before returning
+        v.sort();
+        // create list of account names, sorted by the keys
+        let v_names = (&v)
+            .iter()
+            .map(|&k| self.accounts().get(k).unwrap().name())
+            .collect();
+        return (v, v_names);
+    }
+    /// Add a new account to the configuration
+    pub fn add_account(&mut self, key: &str, props: &toml::Value) -> Result<(), Error> {
+        // create account and push to conf
+        // can't use serialization here for the entire account
+        // because we have a more complex relationship between the Account struct and its components
+        let mut acct = match Account::try_from(props) {
+            Ok(a) => a,
+            Err(e) => return Err(e),
+        };
+        self.accounts.insert(key.to_string(), acct);
+        Ok(())
+    }
+
+    /// Query configuration by the account name or key
+    pub fn query_account(&self, s: &str) -> Option<&Account> {
+        // check `s` against both keys and names
+        let (acct_keys, acct_names) = self.accounts_sorted();
+        match acct_keys.contains(&s) {
+            true => Some(self.accounts().get(s).unwrap()),
+            false => match acct_names.iter().position(|&a| a == s) {
+                Some(idx) => {
+                    let acct_key = acct_keys[idx];
+                    self.accounts().get(acct_key)
+                }
+                None => None,
+            },
+        }
+    }
+}
+
+impl<'a> Display for Config<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Accounts: {:?}", self.accounts_sorted())
+    }
+}
---
+++ src/config/utils.rs
@@ -0,0 +1,53 @@
+//! Utilities to load, parse, and manage the configuration.
+
+use home::home_dir;
+use std::{env, io::Result, path::PathBuf};
+use toml::map::Map;
+use toml::Value;
+
+use crate::config::config::Config;
+
+/// Parse a TOML table for accounts and create Accounts
+pub(crate) fn parse_accounts<'a, 'b>(
+    accounts: &'a Map<String, Value>,
+    conf: &'a mut Config<'b>,
+) -> Result<()> {
+    for (acct, props) in accounts {
+        match conf.add_account(acct, props) {
+            Ok(_) => {}
+            Err(e) => return Err(e),
+        }
+    }
+    Ok(())
+}
+
+/// Check multiple locations for a configuration file and return the highest priority one
+pub fn get_config_path() -> PathBuf {
+    // check for `QUILL_CONFIG` environment variable
+    match env::var("QUILL_CONFIG") {
+        Ok(p) => PathBuf::from(p),
+        Err(_) => {
+            // check if $XDG_CONFIG_HOME is set
+            let mut cfg_path = match env::var("XDG_CONFIG_HOME") {
+                Ok(dir) => PathBuf::from(dir),
+                // if not set, make it the default $HOME/.config
+                Err(_) => {
+                    if let Some(mut dir) = home_dir() {
+                        dir.push(".config");
+                        dir
+                    } else {
+                        PathBuf::new()
+                    }
+                }
+            };
+
+            // get config from within $XDG_CONFIG_HOME
+            cfg_path.push("quill");
+            cfg_path.push("config.toml");
+            match cfg_path.exists() {
+                true => cfg_path,
+                false => PathBuf::from("config.toml"),
+            }
+        }
+    }
+}
