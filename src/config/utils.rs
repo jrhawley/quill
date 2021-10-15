76f76d52f16910f28e836fdafb4311a975e596d9
James Hawley <james@jrhawley.ca>
2021-10-15 10:31:49 -0400

    Changing how the config file is found
    No longer using `home` crate.
    Replaced with similar functionality from `dirs` crate.
    
    Better cross-platform support for home and configuration directories using `dirs` crate.
    
    No longer checking `QUILL_CONFIG` environment variable.
    Just checking the config directories, since they're easily specified on
    all operating systems, now.

--- src/config/utils.rs
+++ src/config/utils.rs
@@ -1,8 +1,7 @@
 //! Utilities to load, parse, and manage the configuration.
 
 use clap::crate_name;
-use dirs;
-use home::home_dir;
+use dirs::{config_dir, home_dir};
 use std::path::Path;
 use std::{env, io::Result, path::PathBuf};
 use toml::map::Map;
@@ -26,32 +25,26 @@ pub(crate) fn parse_accounts<'a, 'b>(
 
 /// Check multiple locations for a configuration file and return the highest priority one
 pub fn get_config_path() -> PathBuf {
-    // check for `QUILL_CONFIG` environment variable
-    match env::var("QUILL_CONFIG") {
-        Ok(p) => PathBuf::from(p),
-        Err(_) => {
-            // check if $XDG_CONFIG_HOME is set
-            let mut cfg_path = match env::var("XDG_CONFIG_HOME") {
-                Ok(dir) => PathBuf::from(dir),
-                // if not set, make it the default $HOME/.config
-                Err(_) => {
-                    if let Some(mut dir) = home_dir() {
-                        dir.push(".config");
-                        dir
-                    } else {
-                        PathBuf::new()
-                    }
-                }
-            };
-
-            // get config from within $XDG_CONFIG_HOME
-            cfg_path.push(crate_name!().to_lowercase());
-            cfg_path.push("config.toml");
-            match cfg_path.exists() {
-                true => cfg_path,
-                false => PathBuf::from("config.toml"),
+    // check if $XDG_CONFIG_HOME is set
+    let mut cfg_path = match config_dir() {
+        Some(dir) => PathBuf::from(dir),
+        // if not set, make it the default $HOME/.config
+        None => {
+            if let Some(mut dir) = home_dir() {
+                dir.push(".config");
+                dir
+            } else {
+                PathBuf::new()
             }
         }
+    };
+
+    // get config from within $XDG_CONFIG_HOME
+    cfg_path.push(crate_name!().to_lowercase());
+    cfg_path.push("config.toml");
+    match cfg_path.exists() {
+        true => cfg_path,
+        false => PathBuf::from("config.toml"),
     }
 }
 
@@ -65,7 +58,7 @@ pub fn expand_tilde<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
     if p == Path::new("~") {
         return home_dir();
     }
-    dirs::home_dir().map(|mut h| {
+    home_dir().map(|mut h| {
         if h == Path::new("/") {
             // base case: `h` root directory;
             // don't prepend extra `/`, just drop the tilde.
