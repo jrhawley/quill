0607097edc1fc1be47533e03ee092690275637b9
James Hawley <james@jrhawley.ca>
2021-10-18 21:59:31 -0400

    Refactoring CLI parsing into its own module
    - Less duplicate code
    - Better legibility

---
+++ src/cli/mod.rs
@@ -0,0 +1,56 @@
+//! Command line interface configuration.
+
+use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg, ArgMatches};
+use std::{io, path::{Path, PathBuf}};
+
+use crate::config::{config::Config, utils::get_config_path};
+
+/// Parse the CLI
+pub(crate) fn cli_extract_cfg<'a>() -> io::Result<Config<'a>> {
+    let cfg_path = get_config_path();
+    let matches = get_cli_matches(cfg_path.as_path());
+    validate_cli(&matches)?;
+    match cli_extract_conf_path(&matches) {
+        Ok(p) => Config::new_from_path(p.as_path()),
+        Err(e) => Err(e)
+    }
+}
+
+/// Get the matches from the CLI
+fn get_cli_matches<'a>(default_cfg: &'a Path) -> ArgMatches<'a> {
+    // convert default config file path into a string for ArgMatches
+    let display_path = default_cfg.to_str().unwrap_or("");
+
+    // CLI interface for binary
+    let matches = app_from_crate!()
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("CONF")
+                .help("The statement configuration file")
+                .takes_value(true)
+                .default_value(display_path),
+        )
+        .get_matches();
+
+    matches
+}
+
+/// Validate the given CLI arguments
+fn validate_cli(matches: &ArgMatches) -> io::Result<()> {
+    // extract the value of the config file path
+    match cli_extract_conf_path(matches) {
+        Ok(p) => {},
+        Err(e) => return Err(e)
+    }
+    Ok(())
+}
+
+/// Extract the configuration file path from the CLI arguments
+fn cli_extract_conf_path(matches: &ArgMatches) -> io::Result<PathBuf> {
+    match matches.value_of("config") {
+        Some(p) => Ok(PathBuf::from(p)),
+        None => return Err(io::Error::new(io::ErrorKind::NotFound, "No configuration file given."))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,23 @@
-use clap::{app_from_crate, crate_authors, crate_description, crate_name, crate_version, Arg};
+use cli::cli_extract_cfg;
 use log::error;
 use simple_logger;
 use std::collections::HashMap;
-use std::path::Path;
-use std::{env, process};
+use std::process;
 
+mod cli;
 mod config;
 mod models;
 mod tui;
-use crate::config::{config::Config, utils::get_config_path};
+use crate::config::config::Config;
 use crate::models::{date::Date, statement::Statement};
 use crate::tui::start_tui;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initiate the log level
-    simple_logger::init().unwrap();
-    // get QUILL_CONFIG environment variable to find location of the default config file
-    let conf_env_path = get_config_path();
-    // CLI interface for binary
-    let matches = app_from_crate!()
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .value_name("CONF")
-                .help("The statement configuration file")
-                .takes_value(true)
-                .default_value(conf_env_path.to_str().unwrap()),
-        )
-        .get_matches();
-
-    // 1. read account configuration
-    // parse CLI args for config file
-    let conf_path = matches.value_of("config").unwrap();
-    let conf = match Config::new_from_path(Path::new(conf_path)) {
-        Ok(cfg) => cfg,
-        Err(_) => {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
-                "Error parsing configuration file.",
-            )))
-        }
-    };
-    println!("{:#?}", conf);
+    simple_logger::init()?;
+    
+    // parse and validate the CLI arguments
+    let conf = cli_extract_cfg()?;
 
     // get a sorted list of account keys
     let acct_order = conf.accounts_sorted().0;
