c38ae25d14fbfd4195152a24e73bd12e0ecf699f
James Hawley <developer@jrhawley.ca>
2021-11-09 15:42:05 -0500

    Refactoring rendering of the "Accounts" tab

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -312,40 +312,6 @@ pub fn start_tui(
     Ok(())
 }
 
-/// Block for rendering "Accounts" page
-fn render_accounts<'a>(conf: &'a Config, acct_order: &'a Vec<&str>) -> Table<'a> {
-    let accts: Vec<Row> = acct_order
-        .iter()
-        .map(|&k| {
-            let acct = conf.accounts().get(k).unwrap();
-            Row::new(vec![
-                acct.name(),
-                acct.institution(),
-                acct.directory().to_str().unwrap_or(""),
-            ])
-        })
-        .collect();
-    let acct_table = Table::new(accts)
-        .header(
-            Row::new(vec!["Account Name", "Institution", "Directory"]).style(
-                Style::default()
-                    .fg(Color::Yellow)
-                    .add_modifier(Modifier::BOLD)
-                    .add_modifier(Modifier::UNDERLINED),
-            ),
-        )
-        .block(Block::default().borders(Borders::ALL))
-        .widths(&[
-            Constraint::Min(20),
-            Constraint::Min(30),
-            Constraint::Min(20),
-        ])
-        .column_spacing(2)
-        .style(Style::default().bg(Color::Black))
-        .highlight_style(Style::default().bg(Color::Blue));
-    acct_table
-}
-
 /// Gracefully close down the TUI
 fn close_tui(
     term: &mut Terminal<CrosstermBackend<Stdout>>,
