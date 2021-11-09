4dfa15d743e0150cece83d8bb095a03a627c8696
James Hawley <developer@jrhawley.ca>
2021-11-09 15:40:24 -0500

    Refactoring rendering of the "Log" tab

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -316,67 +316,6 @@ pub fn start_tui(
     Ok(())
 }
 
-/// Block for rendering "Log" page
-fn render_log<'a>(
-    conf: &'a Config,
-    acct_stmts: &'a HashMap<&str, Vec<(Date, Option<Statement>)>>,
-    acct_order: &'a Vec<&str>,
-    acct_state: &ListState,
-    log_state: &ListState,
-) -> (List<'a>, List<'a>) {
-    let acct_names_ordered: Vec<ListItem> = acct_order
-        .iter()
-        .map(|&a| ListItem::new(conf.accounts().get(a).unwrap().name()))
-        .collect();
-
-    let mut accts = List::new(acct_names_ordered)
-        .block(Block::default().title("Accounts").borders(Borders::ALL))
-        .highlight_style(Style::default().bg(Color::Blue));
-
-    // get the log of statements for the selected account
-    let rows: Vec<ListItem> = match acct_state.selected() {
-        Some(acct_idx) => {
-            // get the HashMap key of the account that's highlighted
-            let acct_key = acct_order[acct_idx];
-            // convert the statements into formatted Rows
-            acct_stmts
-                .get(acct_key)
-                .unwrap()
-                .iter()
-                // go through in reverse chronological order so latest is at the top
-                .rev()
-                .map(|(d, s)| {
-                    ListItem::new(format!(
-                        "{} {}",
-                        d,
-                        match s {
-                            Some(_) => String::from("✔"),
-                            None => String::from("❌"),
-                        }
-                    ))
-                })
-                .collect()
-        }
-        // return the template table if no Account is selected
-        // this should never happen
-        None => vec![ListItem::new("There are no accounts")],
-    };
-    let mut log = List::new(rows)
-        .block(Block::default().title("Statements").borders(Borders::ALL))
-        .highlight_style(Style::default().bg(Color::Blue));
-
-    // dim the side that is not selected
-    if let Some(_) = log_state.selected() {
-        accts = accts.style(Style::default().add_modifier(Modifier::DIM));
-        log = log.style(Style::default());
-    } else {
-        accts = accts.style(Style::default());
-        log = log.style(Style::default().add_modifier(Modifier::DIM));
-    }
-
-    (accts, log)
-}
-
 /// Block for rendering "Accounts" page
 fn render_accounts<'a>(conf: &'a Config, acct_order: &'a Vec<&str>) -> Table<'a> {
     let accts: Vec<Row> = acct_order
---
+++ src/tui/render/log.rs
@@ -0,0 +1,68 @@
+//! Functions for rendering the "Log" page.
+
+use tui::{
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, List, ListItem},
+};
+
+use crate::{config::Config, models::StatementCollection, tui::state::LogState};
+
+/// Create a block to render the "Log" page.
+pub fn log<'a>(
+    conf: &'a Config<'a>,
+    acct_stmts: &StatementCollection,
+    state: &LogState,
+) -> (List<'a>, List<'a>) {
+    let acct_names_ordered: Vec<ListItem> = conf
+        .keys()
+        .iter()
+        .map(|a| ListItem::new(conf.accounts().get(a.as_str()).unwrap().name()))
+        .collect();
+
+    let mut accts = List::new(acct_names_ordered)
+        .block(Block::default().title("Accounts").borders(Borders::ALL))
+        .highlight_style(Style::default().bg(Color::Blue));
+
+    // get the log of statements for the selected account
+    let rows: Vec<ListItem> = match state.selected_account() {
+        Some(acct_idx) => {
+            // get the HashMap key of the account that's highlighted
+            let acct_key = conf.keys()[acct_idx].as_str();
+            // convert the statements into formatted Rows
+            acct_stmts
+                .get(acct_key)
+                .unwrap()
+                .iter()
+                // go through in reverse chronological order so latest is at the top
+                .rev()
+                .map(|(d, s)| {
+                    ListItem::new(format!(
+                        "{} {}",
+                        d,
+                        match s {
+                            Some(_) => String::from("✔"),
+                            None => String::from("❌"),
+                        }
+                    ))
+                })
+                .collect()
+        }
+        // return the template table if no Account is selected
+        // this should never happen
+        None => vec![ListItem::new("There are no accounts")],
+    };
+    let mut log = List::new(rows)
+        .block(Block::default().title("Statements").borders(Borders::ALL))
+        .highlight_style(Style::default().bg(Color::Blue));
+
+    // dim the side that is not selected
+    if let Some(_) = state.selected_log() {
+        accts = accts.style(Style::default().add_modifier(Modifier::DIM));
+        log = log.style(Style::default());
+    } else {
+        accts = accts.style(Style::default());
+        log = log.style(Style::default().add_modifier(Modifier::DIM));
+    }
+
+    (accts, log)
+}
