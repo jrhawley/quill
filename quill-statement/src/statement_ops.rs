8e9f7180e889d38c8e906061e1739ea10c5199a9
James Hawley <james@jrhawley.ca>
2022-02-01 21:32:47 -0500

    Adding `prev_weekday_date` function for stepping back
    The inverse operation of `next_weekday_date`

--- quill-statement/src/statement_ops.rs
+++ quill-statement/src/statement_ops.rs
@@ -1,7 +1,7 @@
 //! Multiple operations for working with `Statements`.
 
-use chrono::{Datelike, Duration, NaiveDate, Weekday};
-use kronos::{Grain, Grains, TimeSequence};
+use chrono::{Datelike, Duration, Local, NaiveDate, Weekday};
+use kronos::{Grain, Grains, Shim, TimeSequence};
 
 /// Calculate the next weekday from a given date
 pub fn next_weekday_date(d: NaiveDate) -> NaiveDate {
@@ -14,6 +14,25 @@ pub fn next_weekday_date(d: NaiveDate) -> NaiveDate {
             .date(),
         Weekday::Sun => Grains(Grain::Day)
             .future(&(d + Duration::days(1)).and_hms(0, 0, 0))
+            .next()
+            .unwrap()
+            .start
+            .date(),
+        _ => d,
+    }
+}
+
+/// Calculate the previous weekday from a given date
+pub fn prev_weekday_date(d: NaiveDate) -> NaiveDate {
+    match d.weekday() {
+        Weekday::Sat => Grains(Grain::Day)
+            .future(&(d - Duration::days(1)).and_hms(0, 0, 0))
+            .next()
+            .unwrap()
+            .start
+            .date(),
+        Weekday::Sun => Grains(Grain::Day)
+            .future(&(d - Duration::days(2)).and_hms(0, 0, 0))
             .next()
             .unwrap()
             .start
@@ -50,5 +69,31 @@ mod tests {
         check_next_weekday_date(sunday, monday);
         check_next_weekday_date(monday, monday);
         check_next_weekday_date(tuesday, tuesday);
+    }
+
+    #[track_caller]
+    fn check_prev_weekday_date(input_date: NaiveDate, expected: NaiveDate) {
+        let observed = prev_weekday_date(input_date);
+
+        assert_eq!(expected, observed);
+    }
+
+    #[test]
+    fn all_prev_weekday_date() {
+        let wednesday = NaiveDate::from_ymd(2021, 12, 1);
+        let thursday = NaiveDate::from_ymd(2021, 12, 2);
+        let friday = NaiveDate::from_ymd(2021, 12, 3);
+        let saturday = NaiveDate::from_ymd(2021, 12, 4);
+        let sunday = NaiveDate::from_ymd(2021, 12, 5);
+        let monday = NaiveDate::from_ymd(2021, 12, 6);
+        let tuesday = NaiveDate::from_ymd(2021, 12, 7);
+
+        check_prev_weekday_date(wednesday, wednesday);
+        check_prev_weekday_date(thursday, thursday);
+        check_prev_weekday_date(friday, friday);
+        check_prev_weekday_date(saturday, friday);
+        check_prev_weekday_date(sunday, friday);
+        check_prev_weekday_date(monday, monday);
+        check_prev_weekday_date(tuesday, tuesday);
     }
 }
