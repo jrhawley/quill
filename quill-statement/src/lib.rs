823ad289f4d8be17e1c6e78b84732ca7b5765814
James Hawley <james@jrhawley.ca>
2022-02-01 23:12:01 -0500

    Refactoring `ignored_statements` module

--- quill-statement/src/lib.rs
+++ quill-statement/src/lib.rs
@@ -5,12 +5,14 @@
 pub(crate) const IGNOREFILE: &str = ".quillignore.toml";
 
 mod ignore_file;
+mod ignored_statements;
 mod observed_statement;
 mod statement_collection;
 mod statement_ops;
 mod statement_status;
 mod statement_struct;
 
+pub use ignored_statements::IgnoredStatements;
 pub use observed_statement::ObservedStatement;
 pub use statement_collection::StatementCollection;
 pub use statement_ops::{
