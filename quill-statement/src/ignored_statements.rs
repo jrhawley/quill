edca5a5bbcd8365116ae6457d879b57c5e14370e
James Hawley <james@jrhawley.ca>
2022-02-10 00:03:44 -0500

    Changing `IgnoredStatements` to a glorified sorted `Vec<NaiveDate>`
    This simplifies a lot of logic when parsing the `IgnoreFile`.
    It also makes it easier to pair up with statements, since the dates should match exactly.

--- quill-statement/src/ignored_statements.rs
+++ quill-statement/src/ignored_statements.rs
@@ -1,68 +1,52 @@
 //! A collection of ignored statements.
 
+use crate::ignore_file::IgnoreFile;
 use chrono::NaiveDate;
-use kronos::Shim;
 use serde::Deserialize;
 use std::slice::Iter;
-
-use crate::ignore_file::IgnoreFile;
-use crate::{expected_statement_dates, pair_dates_statements, Statement, StatementStatus};
+use std::str::FromStr;
 
 /// Control which account statements are ignored.
+/// Essentially a sorted `Vec<NaiveDate>`.
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 pub struct IgnoredStatements {
-    // statement dates that are being skipped/ignored
-    stmts: Vec<Statement>,
+    dates: Vec<NaiveDate>,
 }
 
 impl IgnoredStatements {
     /// Construct an empty `IgnoredStatements` object.
     pub fn empty() -> Self {
-        Self { stmts: vec![] }
-    }
-
-    /// Construct a new `IgnoredStatements` object.
-    pub fn new<'a>(first: &NaiveDate, period: &Shim<'a>, fmt: &str, ignore: &IgnoreFile) -> Self {
-        let stmts_from_dates: Vec<Statement> = match ignore.dates() {
-            Some(d) => d
-                .iter()
-                .filter_map(|d| Statement::try_from((d, fmt)).ok())
-                .collect(),
-            None => vec![],
-        };
-
-        // match the statements from the dates with the required statements
-        let empty_ignore = Self::empty();
-        let required_dates = expected_statement_dates(first, period);
-        let ignored_date_pairing =
-            pair_dates_statements(&required_dates, &stmts_from_dates, &empty_ignore);
-
-        // match the statements from the files with the required statements
-        let mut paired_ignore: Vec<Statement> = vec![];
-        for (i, _) in required_dates.iter().enumerate() {
-            // required_dates, ignored_date_pairing, and ignored_file_pairing
-            // are all in the same order, so we can just deal with indices
-            match ignored_date_pairing[i].status() {
-                // ignore the statement as listed by the date if both are specified
-                StatementStatus::Available => {
-                    paired_ignore.push(ignored_date_pairing[i].statement().clone())
-                }
-                _ => {}
-            }
-        }
-
-        Self::from(paired_ignore)
+        Self { dates: vec![] }
     }
 
     /// Return an iterator over the statements
-    pub fn iter(&self) -> Iter<Statement> {
-        self.stmts.iter()
+    pub fn iter(&self) -> Iter<NaiveDate> {
+        self.dates.iter()
     }
 }
 
-impl From<Vec<Statement>> for IgnoredStatements {
-    fn from(v: Vec<Statement>) -> Self {
-        Self { stmts: v }
+impl From<Vec<NaiveDate>> for IgnoredStatements {
+    fn from(v: Vec<NaiveDate>) -> Self {
+        Self { dates: v }
+    }
+}
+
+impl From<&IgnoreFile> for IgnoredStatements {
+    fn from(ignore: &IgnoreFile) -> Self {
+        match ignore.dates() {
+            Some(v) => {
+                let mut dates: Vec<NaiveDate> = v
+                    .iter()
+                    .filter_map(|d| NaiveDate::from_str(&d.to_string()).ok())
+                    .collect();
+
+                // ensure the list is sorted so iteration over the Vec is the same as moving forward in time
+                dates.sort();
+
+                Self::from(dates)
+            }
+            None => Self::empty(),
+        }
     }
 }
 
@@ -71,7 +55,6 @@ mod tests {
     use std::str::FromStr;
 
     use super::*;
-    use kronos::{Grain, Grains, NthOf};
     use toml::value::Datetime;
 
     #[test]
@@ -79,22 +62,101 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 
-    fn check_new(input: (&NaiveDate, &Shim, &str, &IgnoreFile), expected: IgnoredStatements) {
-        let first = input.0;
-        let period = input.1;
-        let fmt = input.2;
-        let ignore = input.3;
-
-        let observed = IgnoredStatements::new(first, period, fmt, ignore);
+    fn check_from_vec_naivedate(input: Vec<NaiveDate>, expected: IgnoredStatements) {
+        let observed = IgnoredStatements::from(input);
 
         assert_eq!(expected, observed);
     }
 
     #[test]
+    fn from_empty_vec() {
+        let input = vec![];
+        let expected = IgnoredStatements::empty();
+
+        check_from_vec_naivedate(input, expected);
+    }
+
+    #[test]
+    fn from_single_vec() {
+        let single_stmt = vec![NaiveDate::from_ymd(2021, 11, 1)];
+        let input = single_stmt.clone();
+        let expected = IgnoredStatements {
+            dates: single_stmt.clone(),
+        };
+
+        check_from_vec_naivedate(input, expected);
+    }
+
+    #[test]
+    fn from_double_vec() {
+        let double_stmt = vec![
+            NaiveDate::from_ymd(2021, 11, 1),
+            NaiveDate::from_ymd(2022, 12, 1),
+        ];
+        let input = double_stmt.clone();
+        let expected = IgnoredStatements {
+            dates: double_stmt.clone(),
+        };
+
+        check_from_vec_naivedate(input, expected);
+    }
+
+    fn check_new(input: &IgnoreFile, expected: IgnoredStatements) {
+        let observed = IgnoredStatements::from(input);
+
+        assert_eq!(expected, observed);
+    }
+
+    #[test]
+    fn new_empty() {
+        let ignore = IgnoreFile::empty();
+
+        let expected = IgnoredStatements::empty();
+
+        check_new(&ignore, expected);
+    }
+
+    #[test]
+    fn new_single() {
+        let ignore = IgnoreFile::from(vec![Datetime::from_str("2015-07-22").unwrap()]);
+
+        let expected = IgnoredStatements::from(vec![NaiveDate::from_ymd(2015, 7, 22)]);
+
+        check_new(&ignore, expected);
+    }
+
+    #[test]
+    fn new_double_ordered() {
+        let ignore = IgnoreFile::from(vec![
+            Datetime::from_str("2015-07-22").unwrap(),
+            Datetime::from_str("2015-08-22").unwrap(),
+        ]);
+
+        let expected = IgnoredStatements::from(vec![
+            NaiveDate::from_ymd(2015, 7, 22),
+            NaiveDate::from_ymd(2015, 8, 22),
+        ]);
+
+        check_new(&ignore, expected);
+    }
+
+    #[test]
+    fn new_double_unordered() {
+        let ignore = IgnoreFile::from(vec![
+            Datetime::from_str("2015-08-22").unwrap(),
+            Datetime::from_str("2015-07-22").unwrap(),
+        ]);
+
+        let expected = IgnoredStatements::from(vec![
+            NaiveDate::from_ymd(2015, 7, 22),
+            NaiveDate::from_ymd(2015, 8, 22),
+        ]);
+
+        check_new(&ignore, expected);
+    }
+
+    #[test]
     fn realistic_missing() {
-        let first = NaiveDate::from_ymd(2015, 07, 24);
-        let period = Shim::new(NthOf(22, Grains(Grain::Day), Grains(Grain::Month)));
-        let fmt = "%Y-%m-%d";
         let ignore = IgnoreFile::from(vec![
             Datetime::from_str("2021-01-22").unwrap(),
             Datetime::from_str("2021-05-25").unwrap(),
@@ -102,13 +164,13 @@ mod tests {
         ]);
 
         let expected = IgnoredStatements {
-            stmts: vec![
-                Statement::try_from((&NaiveDate::from_ymd(2021, 1, 22), fmt)).unwrap(),
-                Statement::try_from((&NaiveDate::from_ymd(2021, 5, 25), fmt)).unwrap(),
-                Statement::try_from((&NaiveDate::from_ymd(2021, 10, 22), fmt)).unwrap(),
+            dates: vec![
+                NaiveDate::from_ymd(2021, 1, 22),
+                NaiveDate::from_ymd(2021, 5, 25),
+                NaiveDate::from_ymd(2021, 10, 22),
             ],
         };
 
-        check_new((&first, &period, fmt, &ignore), expected);
+        check_new(&ignore, expected);
     }
 }
