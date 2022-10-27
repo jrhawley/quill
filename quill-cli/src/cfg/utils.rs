f11cb2d661a58aaa7193e88730a77b0c4d5a3c78
James Hawley <developer@jrhawley.ca>
2022-10-27 11:20:13 -0400

    Updating binary to clap v4

--- quill-cli/src/cfg/utils.rs
+++ quill-cli/src/cfg/utils.rs
@@ -6,17 +6,23 @@ use dirs_next::{config_dir, home_dir};
 use quill_statement::StatementCollection;
 use std::path::PathBuf;
 
-fn get_config_dir() -> PathBuf {
-    // check if $XDG_CONFIG_HOME is set
+pub(crate) fn get_config_dir() -> Option<PathBuf> {
+    // get config from within $XDG_CONFIG_HOME
     match config_dir() {
-        Some(dir) => dir,
+        Some(mut dir) => {
+            dir.push(crate_name!().to_lowercase());
+
+            Some(dir)
+        },
         // if not set, make it the default $HOME/.config
         None => {
             if let Some(mut dir) = home_dir() {
                 dir.push(".config");
-                dir
+                dir.push(crate_name!().to_lowercase());
+
+                Some(dir)
             } else {
-                PathBuf::new()
+                None
             }
         }
     }
@@ -24,10 +30,8 @@ fn get_config_dir() -> PathBuf {
 
 /// Check multiple locations for a configuration file and return the highest priority one
 pub fn get_config_path() -> PathBuf {
-    let mut cfg_path = get_config_dir();
-
-    // get config from within $XDG_CONFIG_HOME
-    cfg_path.push(crate_name!().to_lowercase());
+    let mut cfg_path = get_config_dir().unwrap();
+    
     cfg_path.push("config.toml");
     match cfg_path.exists() {
         true => cfg_path,
--- quill-cli/src/cli/mod.rs
+++ quill-cli/src/cli/mod.rs
@@ -3,25 +3,22 @@
 use clap::Parser;
 use lazy_static::lazy_static;
 use std::path::{Path, PathBuf};
-
 use crate::cfg::utils::get_config_path;
 
-/// Get the default configuration path used for the CLI
-pub fn default_config_path() -> String {
-    let cfg_path = get_config_path();
-    let cfg_path_str = cfg_path.to_str().unwrap().to_owned();
-
-    cfg_path_str
-}
-
 lazy_static! {
-    static ref DEFAULT_CFG_PATH: String = default_config_path();
+    static ref DEFAULT_CFG_PATH: PathBuf = get_config_path();
 }
 
 #[derive(Debug, Parser)]
 #[clap(author, about, version)]
 pub(crate) struct CliOpts {
-    #[clap(name = "cfg", short, long, help = "Configuration file with accounts and statements info.", default_value = &DEFAULT_CFG_PATH)]
+    #[clap(
+        name = "cfg",
+        short,
+        long,
+        help = "Configuration file with accounts and statements info.",
+        default_value = (*DEFAULT_CFG_PATH).as_os_str()
+    )]
     config: PathBuf,
 }
 
